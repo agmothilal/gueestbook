9727603153fea0f8cfe187f4810e67a6f7eb6744
agmothilal <ag.mothilal@gmail.com>
2021-04-05 18:48:07 -0500

    GREEN: Adding Integration test with controller.

---
+++ src/main/java/com/galvanize/guestbook/controller/GuestBookController.java
@@ -0,0 +1,22 @@
+package com.galvanize.guestbook.controller;
+
+import com.galvanize.guestbook.dto.VisitorCommentDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class GuestBookController {
+
+    private static List<VisitorCommentDto> commentsDto = new ArrayList<>();
+
+    @PostMapping(value = "/comment", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addComment(@RequestBody VisitorCommentDto commentDto) {
+        commentsDto.add(commentDto);
+    }
+
+}
