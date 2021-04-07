fbea5b047e574ac76d3b1b60397784387375a868
cld71 <cld71@cld-pc2.diehl>
2021-04-07 17:23:12 -0500

    Assignment 3 - Spring REST Docs

--- src/test/java/com/galvanize/guestbook/integration/GuestBookIT.java
+++ src/test/java/com/galvanize/guestbook/integration/GuestBookIT.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galvanize.guestbook.dto.VisitorCommentDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class GuestBookIT {
 
     @Autowired
@@ -33,7 +37,8 @@ public class GuestBookIT {
                 content(mapper.writeValueAsString(comment));
 
         mockMvc.perform(requestBuilder)
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(MockMvcRestDocumentation.document("AddComment"));
     }
 
     @Test
@@ -57,7 +62,11 @@ public class GuestBookIT {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("length()").value(1))
                 .andExpect(jsonPath("$[0].name").value("David"))
-                .andExpect(jsonPath("$[0].comment").value("First comment"));
+                .andExpect(jsonPath("$[0].comment").value("First comment"))
+                .andDo(MockMvcRestDocumentation.document("Comments", PayloadDocumentation.responseFields(
+                        PayloadDocumentation.fieldWithPath("[0].name").description("Name of the person that is commenting."),
+                        PayloadDocumentation.fieldWithPath("[0].comment").description("Is the commet.")
+                )));
     }
 
 }
